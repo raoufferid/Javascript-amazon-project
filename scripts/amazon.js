8ee1ab4c6181ff349934fd2ad912357a379acdfc
Raoufferid <raoufferid@gmail.com>
2026-09-13 22:36:23 +0200

    Using the data/products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,28 +1,4 @@
-const products = [ {
-  image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name:'Athletic Cotton Socks - 6 Pairs',
-  priceCents: 1090,
-  rating: {
-    stars: 4.5,
-    count: 87
-  }
-}, {
-  image:'images/products/intermediate-composite-basketball.jpg',
-  name:'Intermediate Size Basketball',
-  priceCents: 2095,
-  rating: {
-    stars: 4,
-    count: 127
-  }
-}, {
-  image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name:'Adults Plain Cotton T-Shirt - 2 Pack',
-  priceCents: 799,
-  rating: {
-    stars: 4.5,
-    count: 56
-  }
-} ]
+
 let html ='';
 products.forEach(product => {
   html+=`
